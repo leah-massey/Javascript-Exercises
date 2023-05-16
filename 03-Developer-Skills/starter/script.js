669e9534641bd11bd06ba7065cc87f872e35ddff
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-05-16 11:03:10 +0100

    code adjusted to receive two parameters (two arrays of temps) and to combine and return temp amplitude

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -12,7 +12,9 @@
 // PROBLEM 1:
 // We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
 
-const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+const t1 = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+const t2 = [1, 2, 3, 10, 20, 45];
 
 // 1) Understanding the problem
 // - what is amplitude? - answer - differnece between highest and lowest
@@ -24,21 +26,21 @@ const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
 // - find min value in array
 // - subtract max from min
 
-const calcTempAmplitude = function (array) {
+const calcTempAmplitude = function (temps1, temps2) {
+  array = temps1.concat(temps2);
+  console.log(array);
   let max = array[0];
   let min = array[0];
 
   for (let i = 0; i < array.length; i++) {
-    if (isNaN(array[i])) {
-      continue;
-    } else if (array[i] > max) {
-      max = array[i];
-    } else if (array[i] < min) {
-      min - array[i];
-    }
+    const curTemp = array[i];
+
+    if (typeof curTemp !== 'number') continue;
+    if (curTemp > max) max = curTemp;
+    if (curTemp < min) min = curTemp;
   }
-  console.log(max - min);
+  console.log(max, min);
   return max - min;
 };
 
-calcTempAmplitude(temperatures);
+console.log(calcTempAmplitude(t1, t2));
