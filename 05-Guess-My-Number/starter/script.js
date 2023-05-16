0f322ba992f1125914e434b60f670e481b20862e
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-05-16 15:28:55 +0100

    Chapter 05 - eventListener added to button

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,11 +1,15 @@
 'use strict';
 
-console.log(document.querySelector('.message').textContent);
+// console.log(document.querySelector('.message').textContent);
 
-document.querySelector('.message').textContent = 'you have won! 🍾';
+// document.querySelector('.message').textContent = 'you have won! 🍾';
 
-document.querySelector('.number').textContent = 12;
-document.querySelector('.score').textContent = 32;
+// document.querySelector('.number').textContent = 12;
+// document.querySelector('.score').textContent = 32;
 
-document.querySelector('.guess').value = 13;
-console.log(document.querySelector('.guess').value);
+// document.querySelector('.guess').value = 13;
+// console.log(document.querySelector('.guess').value);
+
+document.querySelector('.check').addEventListener('click', function () {
+  console.log(document.querySelector('.guess').value);
+});
