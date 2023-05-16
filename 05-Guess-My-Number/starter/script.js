f080e5b024d2d1be7d02f22725f11d575d71b60b
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-05-16 15:15:52 +0100

    Chapter 05 - manipulation of document using .querySelector

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,11 @@
 'use strict';
+
+console.log(document.querySelector('.message').textContent);
+
+document.querySelector('.message').textContent = 'you have won! 🍾';
+
+document.querySelector('.number').textContent = 12;
+document.querySelector('.score').textContent = 32;
+
+document.querySelector('.guess').value = 13;
+console.log(document.querySelector('.guess').value);
