88a3025b22d327618403379f187fe75e32970baf
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-05-16 15:44:12 +0100

    Chapter 05 - if/else statement added for multiple scenarios, secretNumber generated using Math.random

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,6 +10,19 @@
 // document.querySelector('.guess').value = 13;
 // console.log(document.querySelector('.guess').value);
 
+const secretNumber = Math.trunc(Math.random() * 20);
+console.log(`this is the secretnumber: ${secretNumber}`);
+
 document.querySelector('.check').addEventListener('click', function () {
-  console.log(document.querySelector('.guess').value);
+  const guess = Number(document.querySelector('.guess').value);
+  console.log(guess, typeof guess);
+
+  if (!guess) {
+    document.querySelector('.message').textContent = 'not valid entry 🚫';
+  } else if (guess === secretNumber) {
+    document.querySelector('.message').textContent =
+      'Correct! You have won a point! 🍾';
+  } else {
+    document.querySelector('.message').textContent = 'Good try, not this time';
+  }
 });
