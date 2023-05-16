e30338503f66e65b130874346fdd752422f0aa1e
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-05-16 10:22:01 +0100

    thermostat exercise problem setup

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -5,3 +5,21 @@ const x = 23;
 
 const calcAge = birthYear => 2037 - birthYear;
 console.log('up and running!');
+
+/////////////////////////////////
+//EXERCISE - thermostat
+
+// PROBLEM 1:
+// We work for a company building a smart home thermometer. Our most recent task is this: "Given an array of temperatures of one day, calculate the temperature amplitude. Keep in mind that sometimes there might be a sensor error."
+
+const temperatures = [3, -2, -6, -1, 'error', 9, 13, 17, 15, 14, 9, 5];
+
+// 1) Understanding the problem
+// - what is amplitude? - answer - differnece between highest and lowest
+// - what is a sensor error? What to do?
+
+// 2) Breaking into sub problems
+// - how to deal with errors
+// - find max value in array
+// - find min value in array
+// - subtract max from min
