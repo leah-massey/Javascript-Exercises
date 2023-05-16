a28dc68fe6b753ce6ce05e4e3da7303345de0d4f
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-05-16 11:34:24 +0100

    Print forecast challenge set up. Broblem broken down into sub problems

---
+++ 03-Developer-Skills/starter/print-forecast.js
@@ -0,0 +1,27 @@
+'use strict';
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
+
+/*
+// 1) Understanding the problem
+// array to be transformed into a string, seperated by '...'
+// what is the day ? Index +1
+
+// 2) break into sub problems 
+// - transform element to strings 
+// - insert the extra text
+// - join all elements into one string
+// log string to console 
+
+
