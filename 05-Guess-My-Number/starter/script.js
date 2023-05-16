913baa2af174f026fc85d232a4b502e1a41f0362
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-05-16 18:24:34 +0100

    Chapter 05 - score updating on browser as game is played. Updates to UI when player wins

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,19 +10,39 @@
 // document.querySelector('.guess').value = 13;
 // console.log(document.querySelector('.guess').value);
 
-const secretNumber = Math.trunc(Math.random() * 20);
-console.log(`this is the secretnumber: ${secretNumber}`);
+const secretNumber = Math.trunc(Math.random() * 20) + 1;
+console.log(secretNumber);
+let score = 20;
 
 document.querySelector('.check').addEventListener('click', function () {
   const guess = Number(document.querySelector('.guess').value);
   console.log(guess, typeof guess);
 
+  //when there is no input
   if (!guess) {
     document.querySelector('.message').textContent = 'not valid entry 🚫';
+
+    //when playber wins
   } else if (guess === secretNumber) {
     document.querySelector('.message').textContent =
       'Correct! You have won a point! 🍾';
+    score++;
+    document.querySelector('.number').textContent = secretNumber;
+    document.querySelector('.score').textContent = score;
+    //change colour when player wins
+    document.querySelector('body').style.backgroundColor = '#60b347';
+    //change size of number box when player wins
+    document.querySelector('.number').style.width = '30rem';
+    //when player doesn't win
   } else {
-    document.querySelector('.message').textContent = 'Good try, not this time';
+    if (score > 1) {
+      document.querySelector('.message').textContent =
+        'Good try, not this time';
+      score--;
+    } else {
+      document.querySelector('.message').textContent = 'Game over, start again';
+      score = 0;
+    }
+    document.querySelector('.score').textContent = score;
   }
 });
