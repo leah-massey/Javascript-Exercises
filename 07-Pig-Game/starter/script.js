75aeaeb317d5dfff06a30bc7b45c4b7872f188b0
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-05-22 11:23:50 +0100

    07 - pig game. Random number generated for dice roll and corresponding graphic revealed (showing a dice '2' when a random number 2 is generated

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -4,8 +4,21 @@
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
 const diceEl = document.querySelector('.dice');
+const btnNew = document.querySelector('.btn--new');
+const btnRoll = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
 //starting conditions
 score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
+
+//rolling of dice
+
+btnRoll.addEventListener('click', function () {
+  //generate a random dice roll number
+  const diceRoll = Math.trunc(Math.random() * 6) + 1;
+  //display the dice
+  diceEl.classList.remove('hidden');
+  diceEl.src = `dice-${diceRoll}.png`;
+});
