5db4e3f243cf24ed7ee07b2f5317ab5a0b3b899e
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-05-22 16:37:30 +0100

    Score now accumulates each time dice is rolled.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,10 +3,14 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+
+let currentScore = 0;
 
 //starting conditions
 score0El.textContent = 0;
@@ -18,7 +22,14 @@ diceEl.classList.add('hidden');
 btnRoll.addEventListener('click', function () {
   //generate a random dice roll number
   const diceRoll = Math.trunc(Math.random() * 6) + 1;
+  console.log(diceRoll);
   //display the dice
   diceEl.classList.remove('hidden');
   diceEl.src = `dice-${diceRoll}.png`;
+
+  if (diceRoll !== 1) {
+    currentScore += diceRoll;
+    current0El.textContent = currentScore; //change later
+  } else {
+  }
 });
