2eef9c8e27e2bfcaab35f8d6f8228832d7364210
leah-massey <15648177+leah-massey@users.noreply.github.com>
2023-05-23 10:57:13 +0100

    Functionality of buttons deactivated once a player has reached a score of 20 (ie won).

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,14 +3,26 @@
 // Selecting elements
 const score0El = document.querySelector('#score--0');
 const score1El = document.getElementById('score--1');
-const current0El = document.querySelector('#current--0');
-const current1El = document.querySelector('#current--1');
+const current0 = document.getElementById('current--0');
+const current1 = document.querySelector('#current--1');
 const diceEl = document.querySelector('.dice');
 const btnNew = document.querySelector('.btn--new');
 const btnRoll = document.querySelector('.btn--roll');
 const btnHold = document.querySelector('.btn--hold');
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 
+const scores = [0, 0];
+const switchPlayer = function () {
+  document.getElementById(`current--${activePlayer}`).textContent = 0;
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  currentScore = 0;
+};
 let currentScore = 0;
+let activePlayer = 0;
+let playing = true;
 
 //starting conditions
 score0El.textContent = 0;
@@ -20,16 +32,46 @@ diceEl.classList.add('hidden');
 //rolling of dice
 
 btnRoll.addEventListener('click', function () {
-  //generate a random dice roll number
-  const diceRoll = Math.trunc(Math.random() * 6) + 1;
-  console.log(diceRoll);
-  //display the dice
-  diceEl.classList.remove('hidden');
-  diceEl.src = `dice-${diceRoll}.png`;
+  if (playing) {
+    //generate a random dice roll number
+    const diceRoll = Math.trunc(Math.random() * 6) + 1;
+    console.log(diceRoll);
+    //display the dice
+    diceEl.classList.remove('hidden');
+    diceEl.src = `dice-${diceRoll}.png`;
 
-  if (diceRoll !== 1) {
-    currentScore += diceRoll;
-    current0El.textContent = currentScore; //change later
-  } else {
+    if (diceRoll !== 1) {
+      currentScore += diceRoll;
+      document.getElementById(`current--${activePlayer}`).textContent =
+        currentScore;
+    } else {
+      //switch players using ternary operator
+
+      switchPlayer();
+    }
+  }
+});
+
+btnHold.addEventListener('click', function () {
+  if (playing) {
+    // add current score to active player
+    scores[activePlayer] += currentScore;
+    document.getElementById(`score--${activePlayer}`).textContent =
+      scores[activePlayer];
+
+    //check if score >= 100
+    if (scores[activePlayer] >= 20) {
+      //game ends you are winner, game ends
+      playing = false;
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner');
+      document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active');
+    } else {
+      //switch to next player
+      switchPlayer();
+    }
   }
 });
